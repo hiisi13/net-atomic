18d9af7d0321276d5f6d2d3251b773257fcba873
Dmitry Kozhedubov <dmitry-kozhedubov@yandex.ru>
2013-09-04 12:25:19 +0400

    Added nuspec and updated info

--- NetAtomic/Properties/AssemblyInfo.cs
+++ NetAtomic/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NetAtomic")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Atomic reference implementation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dmitry Kozhedubov")]
 [assembly: AssemblyProduct("NetAtomic")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
